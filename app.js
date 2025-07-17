3f7ab58ba62594b62655a0afbe62e2e1c845e296
rh <rh@a.com>
2025-07-17 18:57:53 +0500

    `Update allowed origins in CORS configuration and fix typo in route path`

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(
     origin: [
       "http://localhost:3006",
       "http://127.0.0.1:5500",
-      "http://localhost:5174",
+      "http://localhost:5173",
       "null",
     ], // Allow file:// protocol
     credentials: true,
--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,10 @@ import { addProduct, getAllProducts, deleteProduct } from "../controllers/produc
 
 const router = express.Router();
 
-// POST /api/products - Add a new product
 router.post("/addproducts", addProduct);
 
-// GET /api/products - Get all products
 router.get("/allproducts", getAllProducts);
 
-// DELETE /api/products/:id - Delete a product by ID
 router.delete("/delete-products:id", deleteProduct);
 
 export default router;
