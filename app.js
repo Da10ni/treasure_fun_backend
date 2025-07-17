8ed50ffc0c87ca491cecbbf0624f8be3090b52fe
Shaeroniya <shaeroniya@gmail.com>
2025-07-17 13:43:39 +0500

    changes

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ const PORT = process.env.PORT || 3000;
 app.use(
   cors({
     origin: [
-      "http://localhost:3002",
+      "http://localhost:3000",
       "http://127.0.0.1:5500",
-      "http://localhost:5177",
+      "http://localhost:5173",
       "null",
     ], // Allow file:// protocol
     credentials: true,
