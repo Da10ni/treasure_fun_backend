10fd620ab9fd17c3d3310f6df94e9c2ebab7b7ae
rh <rh@a.com>
2025-07-24 13:52:21 +0500

    changes cors

--- app.js
+++ app.js
@@ -18,12 +18,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ["*"], // Allow file:// protocol
-    credentials: true,
-  })
-);
+
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
